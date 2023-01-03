a5da4faa2084623dfae847e053aa892f7c32ed35
Siddharth Mishra <siddharthmishra2703@gmail.com>
2023-01-04 02:52:50 +0530

    Final Project Files

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+////////////////////////////////Intialization of variables/////////////////////////////////////////////
+//importing songs file
 import songsList from "./songs.js";
+
+//using jquery to select HTML elements for dynamic changing
 const musicPlayer = $("#music_player");
 const songsListDiv = $("#songs_list"); 
 const openButton = $("#songs_list_open");
@@ -17,10 +21,25 @@ const currentProgress = $("#current_progress");
 const currentTime = $("#current_time");
 const maxDuration = $("#max_duration");
 
+
+
+//initial values for variables
 let loop = true;
+let shuffle = false;
+var arr = [0,1,2,3,4];
 let songNo = 0;
 let song = new Audio(songsList[songNo].link);
 
+
+
+
+
+
+
+
+///////////////////////////////////////////////functions///////////////////////////////////////////////
+
+//format time
 const timeFormatter = (timeInput) => {
     let minute = Math.floor(timeInput / 60);
     minute = minute < 10 ? "0" + minute : minute;
@@ -28,12 +47,21 @@ const timeFormatter = (timeInput) => {
     second = second < 10 ? "0" + second : second;
     return `${minute}:${second}`;
 };
+
+//change song duration acc to songs after matadata is loaded
 song.onloadedmetadata = () => {
     maxDuration.text(timeFormatter(song.duration));
 };
+
+//used to set in different functions
 function setSong(songIndex) {
     if(songIndex > 4){
-        songNo = 0;
+        if(loop){
+            songNo = 0;
+        }else{
+            pauseSong();
+            return false;
+        }
     }
     else if(songIndex < 0){
         songNo = 4;
@@ -41,27 +69,32 @@ function setSong(songIndex) {
     else{
         songNo = songIndex;
     }
-    song = new Audio(songsList[songNo].link);
-    songImg.attr("src", songsList[songNo].image);
-    songName.text(songsList[songNo].name);
-    songSinger.text(songsList[songNo].artists);
+    let SongNo = arr[songNo];
+    song = new Audio(songsList[SongNo].link);
+    songImg.attr("src", songsList[SongNo].image);
+    songName.text(songsList[SongNo].name);
+    songSinger.text(songsList[SongNo].artists);
     song.onloadedmetadata = () => {
         maxDuration.text(timeFormatter(song.duration));
     };
+    return true;
 }
 
+//to play song
 function playSong(){
     song.play();
     playButton.addClass("hide");
     pauseButton.removeClass("hide");
 }
 
+//to pause song
 function pauseSong(){
     song.pause();
     pauseButton.addClass("hide");
     playButton.removeClass("hide");
 }
 
+//to create dynamic playlist of songs after open button for playlist is clicked
 function dynamicPlaylist(){
     let i = 1;
     songsList.forEach(song => {
@@ -77,9 +110,7 @@ function dynamicPlaylist(){
 
 
 
-
-
-
+//////////////////////////////////////////////////Event Handeling///////////////////////////////////////
 openButton.click(function(){
     musicPlayer.addClass("hide");
     songsListDiv.removeClass("hide");
@@ -111,13 +142,46 @@ backButton.click(function(){
     playSong();
 });
 
+repeatButton.click(function(){
+    if(loop){
+        loop = false;
+        repeatButton.removeClass("click");
+        repeatButton.addClass("not_click");
+    }else{
+        loop = true;
+        repeatButton.removeClass("not_click");
+        repeatButton.addClass("click");
+    }
+});
 
-
-
-
-
-
-
+shuffleButton.click(function(){
+    pauseSong();
+    //intializing default arr if shuffle button is not clicked and other variables to 0
+    if(shuffle){
+        songNo = 0;
+        arr = [0,1,2,3,4];
+        shuffle = false;
+        shuffleButton.removeClass("click");
+        shuffleButton.addClass("not_click");
+        setSong(0);
+    }else{
+        //intializing randomized arr if shuffle button is not clicked and other variables to 0
+        songNo = 0;
+        arr = [];
+        while(arr.length < 5){
+            var r = Math.floor(Math.random() * 5);
+            if(arr.indexOf(r) === -1) {
+                arr.push(r);
+            }
+        }
+        shuffle = true;
+        shuffleButton.removeClass("not_click");
+        shuffleButton.addClass("click");
+        setSong(0);
+    }
+    playSong();
+    console.log(arr, shuffle);
+});
 
 
 
@@ -139,13 +203,15 @@ progressBar.click(function(event){
     song.currentTime = progress * song.duration;
 });
 
-//update progress every second
+//update progress and current time every second
 setInterval(() => {
     currentTime.text(timeFormatter(song.currentTime));
     currentProgress.css("width", song.currentTime / song.duration.toFixed(3) * 100 + "%");
     if(song.currentTime === song.duration){
         pauseSong();
-        setSong(songNo + 1);
-        playSong();
+        //checking if loop is on or off by the return value of setSong func
+        if(setSong(songNo + 1)){
+            playSong();
+        }
     }
 }, 1000);
